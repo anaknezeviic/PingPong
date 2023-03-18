6074a10f8360ff62f45894530c12141c2842c2ef
anaknezeviic <anaknezevic0500@gmail.com>
2023-03-18 23:03:27 +0100

    igrica

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 200.0f;
+    private Rigidbody2D rb;
+    
+    private void Awake(){
+    	rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Start(){
+    	ResetPosition();
+    	AddStartingForce();
+    }
+    
+     public void ResetPosition(){
+    	rb.position = Vector3.zero;
+    	rb.velocity = Vector3.zero;
+    	
+    	
+    }
+    
+    public void AddStartingForce(){
+    	float x = Random.value < 0.5f ? -1.0f : 1.0f;
+    	float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : 
+    						Random.Range(0.5f, 1.0f);
+    	
+    	Vector2 direction = new Vector2(x, y);
+    	rb.AddForce(direction * this.speed);
+    }
+    
+    public void AddForce(Vector2 force){
+    	rb.AddForce(force);
+    }
+    
+   
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 10.0f;
+    protected Rigidbody2D rb;
+    
+    private void Awake(){
+    	rb = GetComponent<Rigidbody2D>();
+    }
+    
+    public void ResetPosition(){
+    	rb.position = new Vector2(rb.position.x, 0.0f);
+    	rb.velocity = Vector2.zero;
+    }
+}
